e0175d5197095209ee410195a7befeb4da29818b
davidelong0 <davidelong0@hotmail.com>
2025-04-22 18:21:12 +0200

    componente ecperience pronto a ricevere id

--- BW-LINKEDIN/src/App.jsx
+++ BW-LINKEDIN/src/App.jsx
@@ -1,13 +1,35 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+import Experiences from "./Components/Experiences";
+import { useState } from "react";
 
 function App() {
+  // se l'id è quello dell'utente autenticato ti permetterà di modificare altrimenti no
+  const [selectedUserId, setSelectedUserId] = useState("6807642cd451810015ce83d6"); // Imposta un ID di default o uno iniziale
+  // ID dell'utente autenticato
+  const [authenticatedUserId, setAuthenticatedUserId] = useState("6807642cd451810015ce83d6"); // Imposta l'ID dell'utente autenticato
+
+  
+  const handleUserSearch = (e) => {
+    setSelectedUserId(e.target.value);  
+  };
+
   return (
     <>
-      <header></header>
-      <main></main>
+      <header>
+        
+      </header>
+
+      <main>
+        
+        <Experiences 
+          userId={selectedUserId} 
+          authenticatedUserId={authenticatedUserId} 
+        />
+      </main>
+
       <footer></footer>
     </>
   );
