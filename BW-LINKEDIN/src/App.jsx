1216c1b879523919abf470535cf4cab77923eb53
AngeloZorzi <astro.drg666@gmail.com>
2025-04-22 16:42:58 +0200

    Creates ProfileCard.jsx and some css

--- BW-LINKEDIN/src/App.jsx
+++ BW-LINKEDIN/src/App.jsx
@@ -2,12 +2,15 @@
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import "./App.css";
+import ProfileCard from "./Components/ProfileCard";
 
 function App() {
   return (
     <>
       <header></header>
-      <main></main>
+      <main>
+        <ProfileCard />
+      </main>
       <footer></footer>
     </>
   );
